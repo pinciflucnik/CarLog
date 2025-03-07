a4fc90198f738d7291d34ad2ec599a88b5f23e9a
Vladimir Dimitrov <pinciflucnik1@gmail.com>
2025-03-07 17:57:20 +0200

    change home message

--- CarLog/src/components/home/Home.jsx
+++ CarLog/src/components/home/Home.jsx
@@ -2,9 +2,9 @@ export default function Home() {
     return (
         <div className='my-wrapper'>
             <div className="welcome-hero-txt" >
-                <h2>get your desired car in resonable price</h2>
+                <h2>Your Ultimate Vehicle Maintenance Tracker</h2>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore   magna aliqua.
+                Keeping track of your car’s fuel consumption, repairs, and maintenance has never been easier! UpSpoken is designed to help you log refuelings, track repairs, and monitor servicing costs effortlessly.
                 </p>
                 <button className="welcome-btn" >contact us</button>
             </div>
