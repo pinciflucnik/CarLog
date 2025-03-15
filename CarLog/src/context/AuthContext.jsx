2f05a2dd38baaee2ed43a2d2bc6755dc153f1bff
pinciflucnik <pinciflucnik1@gmail.com>
2025-03-15 11:02:16 +0200

    add persisted state

--- CarLog/src/context/AuthContext.jsx
+++ CarLog/src/context/AuthContext.jsx
@@ -1,13 +1,24 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
-    const [auth, setAuth] = useState({});
+    const [auth, setAuth] = useState(()=> {
+        const persistedAuth = sessionStorage.getItem('auth');
+        if(!persistedAuth){
+            return {}
+        }
+
+        return JSON.parse(persistedAuth)
+    });
 
 
     const authSetter = (data) => {
         setAuth(data)
+        if(!data.email){
+            return sessionStorage.removeItem('auth')
+        }
+        sessionStorage.setItem('auth', JSON.stringify(data))
     }
     
     console.log(auth);
